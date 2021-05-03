fd0a552207e903b21badd34c8d066ee0fd7c9da4
RomainSimplon <77976732+RomainSimplon@users.noreply.github.com>
2021-05-03 10:07:55 +0200

    Add files via upload

---
+++ tp_final_poo/index.php
@@ -0,0 +1,14 @@
+<?php
+include 'interface_utilisateur/structure/header.php';
+?>
+ <h1>Destinations</h1>
+
+<h3>Choisissez votre destination :</h3>
+
+<ul>
+<li><a href="">Grenoble</a></li>
+<li><a href="">Costa Rica</a></li>
+<li><a href="">Malte</a></li>
+<li><a href="">Ibiza</a></li>
+<li><a href="">Zurich (Premium)</a></li>
+</ul>
